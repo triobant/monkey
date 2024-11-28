abf71670a92af09b22ec7d87d8e52dbcf260e015
Brian Ott <bott@b1-systems.de>
2024-11-28 21:45:05 +0100

    update: token with func LoopupIdent that checks the keywords table

--- token/token.go
+++ token/token.go
@@ -37,3 +37,10 @@ var keywords = map[string]TokenType{
     "fn":   FUNCTION,
     "let":  LET,
 }
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
