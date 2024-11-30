e1bc997d129327cd1ea087e4072ce70bcf1cb0e9
Brian Ott <bott@b1-systems.de>
2024-11-30 17:22:55 +0100

    update: lexer to call skipWhitespace; skipWhitespace helper function

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -19,6 +19,8 @@ func New(input string) *Lexer {
 func (l *Lexer) NextToken() token.Token {
     var tok token.Token
 
+    l.skipWhitespace()
+
     switch l.ch {
     case '=':
         tok = newToken(token.ASSIGN, l.ch)
@@ -51,6 +53,12 @@ func (l *Lexer) NextToken() token.Token {
 
     l.readChar()
     return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+    for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+        l.readChar()
+    }
 }
 
 func (l *Lexer) readChar() {
