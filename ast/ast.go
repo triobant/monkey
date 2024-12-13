e88d1192b38465143ac6963b0d516d110ece582e
Brian Ott <bott@b1-systems.de>
2024-12-13 22:18:46 +0100

    update: methods statementNode and TokenLiteral for Identifier struct

--- ast/ast.go
+++ ast/ast.go
@@ -33,10 +33,13 @@ type LetStatement struct {
     Value   Expression
 }
 
-func (ls *LetStatement) statementNode()         {}
-func (ls *LetStatement) TokenLiteral()          { return ls.Token.Literal() }
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal() }
 
 type Identifier struct {
     Token   token.Token // the token.IDENT token
     Value   string
 }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal() }
