47a5c2ed79e3ebf956140fdc2cc2c886eb3e45de
Brian Ott <bott@b1-systems.de>
2024-12-16 22:28:29 +0100

    update: New function calling nextToken

--- parser/parser.go
+++ parser/parser.go
@@ -12,3 +12,13 @@ type Parser struct {
     curToken    token.Token
     peekToken   token.Token
 }
+
+func New(l *lexer.Lexer) *Parser {
+    p := &Parser{l: l}
+
+    // Read two tokens, so curToken and peekToken are both set
+    p.nextToken()
+    p.nextToken()
+
+    return p
+}
