9c160e8059df255cd72cc0b46300bd53e1efbb40
Brian Ott <bott@b1-systems.de>
2025-01-10 20:18:14 +0100

    update: defined methods to fulfill Node and Statement interfaces

--- ast/ast.go
+++ ast/ast.go
@@ -50,3 +50,6 @@ type ReturnStatement struct {
     Token       token.Token // the 'return' token
     ReturnValue Expression
 }
+
+func (rs *ReturnStatement) statementNode()      {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
