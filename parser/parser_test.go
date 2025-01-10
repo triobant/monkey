1c1866f9009be413f4a234e387ab5d74de9fc676
Brian Ott <bott@b1-systems.de>
2025-01-10 16:53:28 +0100

    update: printing test errors and stopping exec of current test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -73,4 +73,10 @@ func checkParserErrors(t *testing.T, p *Parser) {
     if len(errors) == 0 {
         return
     }
+
+    t.Errorf("parser has %d errors", len(errors))
+    for _, msg := range errors {
+        t.Errorf("parser error: %q", msg)
+    }
+    t.FailNow()
 }
