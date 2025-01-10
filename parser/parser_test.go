e95e4bfea0f1415c34ff21912ad08421a87cf70c
Brian Ott <bott@b1-systems.de>
2025-01-10 16:51:23 +0100

    update: check if there are any errors

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -69,4 +69,8 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
+    errors := p.Errors()
+    if len(errors) == 0 {
+        return
+    }
 }
