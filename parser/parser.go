8480efb07c607a63b08f069377fd8c5005f327e3
Brian Ott <bott@b1-systems.de>
2025-01-13 17:12:05 +0100

    update: case to call parseReturnStatement

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func (p *Parser) parseStatement() ast.Statement {
     switch p.curToken.Type {
     case token.LET:
         return p.parseLetStatement()
+    case token.RETURN:
+        return p.parseReturnStatement()
     default:
         return nil
     }
