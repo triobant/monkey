74c0eeb1426081d16a9866691accbf962f927e7a
Brian Ott <bott@b1-systems.de>
2025-01-14 15:29:35 +0100

    update: loop through until semicolon - return stmt

--- parser/parser.go
+++ parser/parser.go
@@ -89,6 +89,13 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
     stmt := &ast.ReturnStatement{Token: p.curToken}
 
     p.nextToken()
+    // TODO: We're skipping the expressions until we
+    // encounter a semicolon
+    for !p.curTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+
+    return stmt
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
