b032360a61df64ff9190ca46ca8faa70d3bb37a8
Brian Ott <bott@b1-systems.de>
2025-01-20 21:13:46 +0100

    update: constructed ast for demo purposes

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -6,4 +6,19 @@ import (
 )
 
 func TestString(t *testing.T) {
+    program := &program{
+        Statements: []Statement{
+            &LetStatement{
+                Token: token.Token{Type: token.LET, Literal: "let"},
+                Name: &Identifier{
+                    Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+                    Value: "myVar",
+                },
+                Value: &Identifier{
+                    Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+                    Value: "anotherVar",
+                },
+            },
+        },
+    }
 }
