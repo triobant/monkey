78b7d4999699ec844efc2d46fd878fae05caa87c
Brian Ott <bott@b1-systems.de>
2025-01-20 16:03:59 +0100

    update: re-arranging String method for LetStatement

--- ast/ast.go
+++ ast/ast.go
@@ -51,6 +51,22 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+    var out bytes.Buffer
+
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
+
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
+}
+
 
 type Identifier struct {
     Token   token.Token // the token.IDENT token
@@ -75,22 +91,6 @@ type ExpressionStatement struct {
 
 func (es *ExpressionStatement) statementNode()  {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
-
-func (ls *LetStatement) String() string {
-    var out bytes.Buffer
-
-    out.WriteString(ls.TokenLiteral() + " ")
-    out.WriteString(ls.Name.String())
-    out.WriteString(" = ")
-
-    if ls.Value != nil {
-        out.WriteString(ls.Value.String())
-    }
-
-    out.WriteString(";")
-
-    return out.String()
-}
 
 func (rs *ReturnStatement) String() string {
     var out bytes.Buffer
