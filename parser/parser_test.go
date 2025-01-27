600daead5bb612b2a19e2ad78b05be51454adb7b
Brian Ott <bott@b1-systems.de>
2025-01-27 14:41:50 +0100

    update: fixed indent for loop

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -189,28 +189,28 @@ func TestParsingPrefixExpressions(t *testing.T) {
         p := New(l)
         program := p.parseProgram()
         checkParserErrors(t, p)
-    }
 
-    if len(program.Statements) != 1 {
-        t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
-            1, len(program.Statements))
-    }
+        if len(program.Statements) != 1 {
+            t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+                1, len(program.Statements))
+        }
 
-    stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
-    if !ok {
-        t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
-            program.Statements[0])
-    }
+        stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+        if !ok {
+            t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+                program.Statements[0])
+        }
 
-    exp, ok := stmt.Expression.(*ast.PrefixExpression)
-    if !ok {
-        t.Fatalf("stmt is not ast.PrefixExpression. got=%T", stmt.Expression)
-    }
-    if exp.Operator != tt.operator {
-        t.Fatalf("exp.Operator is not '%s'. got=%s",
-            tt.operator, exp.Operator)
-    }
-    if !testIntegerLiteral(t, exp.Right, tt.integerValue) {
-        return
+        exp, ok := stmt.Expression.(*ast.PrefixExpression)
+        if !ok {
+            t.Fatalf("stmt is not ast.PrefixExpression. got=%T", stmt.Expression)
+        }
+        if exp.Operator != tt.operator {
+            t.Fatalf("exp.Operator is not '%s'. got=%s",
+                tt.operator, exp.Operator)
+        }
+        if !testIntegerLiteral(t, exp.Right, tt.integerValue) {
+            return
+        }
     }
 }
