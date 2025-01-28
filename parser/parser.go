e7dcc1818b4af9dfccdf26c080b9d8ddd99c58fc
Brian Ott <bott@b1-systems.de>
2025-01-28 15:33:56 +0100

    update: method parsePrefixExpression build ast node *ast.PrefixExpression and advancing tokens

--- parser/parser.go
+++ parser/parser.go
@@ -173,6 +173,16 @@ func (p *Parser) parseIntegerLiteral() ast.Expression {
 }
 
 func (p *Parser) parsePrefixExpression() ast.Expression {
+    expression := &ast.PrefixExpression{
+        Token:      p.curToken,
+        Operator:   p.curToken.Literal,
+    }
+
+    p.nextToken()
+
+    expression.Right = p.parseExpression(PREFIX)
+
+    return expression
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
