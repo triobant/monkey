c82cc17fc597db42e3930d1848c506b6f0877802
Brian Ott <bott@b1-systems.de>
2025-01-30 16:06:58 +0100

    update: String method for InfixExpression returning string

--- ast/ast.go
+++ ast/ast.go
@@ -150,4 +150,13 @@ type InfixExpression struct {
 func (ie *InfixExpression) expressionNode()     {}
 func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
 func (ie *InfixExpression) String() string {
+    var out bytes.Buffer
+
+    out.WriteString("(")
+    out.WriteString(ie.left.String())
+    out.WriteString(" " + io.Operator + " ")
+    out.WriteString(ie.Right.String())
+    out.WriteString(")")
+
+    return out.String()
 }
