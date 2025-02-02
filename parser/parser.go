d1c10376588841ade32d3b942adc2d81ddbb8089
Brian Ott <bott@b1-systems.de>
2025-02-02 22:35:33 +0100

    update: fixed missing lt symbol

--- parser/parser.go
+++ parser/parser.go
@@ -171,7 +171,7 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
     }
     leftExp := prefix()
 
-    for !p.peekTokenIs(token.SEMICOLON) && precedence p.peekPrecedence() {
+    for !p.peekTokenIs(token.SEMICOLON) && precedence < p.peekPrecedence() {
         infix := p.infixParseFns[p.peekToken.Type]
         if infix == nil {
             return leftExp
