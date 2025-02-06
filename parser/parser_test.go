b6840f5104a4b0e346ad8a277d73a3bc13c15f42
Brian Ott <bott@b1-systems.de>
2025-02-06 18:25:03 +0100

    update: changing TestReturnStatements to use helper methods and make it simpler

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -42,6 +42,42 @@ func TestLetStatements(t *testing.T) {
     }
 }
 
+func TestReturnStatements(t *testing.T) {
+    tests := []struct {
+        input           string
+        expectedValue   interface{}
+    }{
+        {"return 5;", 5},
+        {"return true;", true},
+        {"return foobar;", "foobar"},
+    }
+
+    for _, tt := range tests {
+        l := lexer.New(tt.input)
+        p := New(l)
+        program := p.ParseProgram()
+        checkParserErrors(t, p)
+
+        if len(program.Statements) != 1 {
+            t.Fatalf("program.Statements does not contain 1 statements. got=%d",
+                len(program.Statements))
+        }
+
+        stmt := program.Statements[0]
+        returnStmt, ok := stmt.(*ast.ReturnStatement)
+        if !ok {
+            t.Fatalf("stmt not *ast.ReturnStatement. got=%T", stmt)
+        }
+        if returnStmt.TokenLiteral() != "return" {
+            t.Fatalf("returnStmt.TokenLiteral not 'return', got %q",
+                returnStmt.TokenLiteral())
+        }
+        if testLiteralExpression(t, returnStmt.ReturnValue, tt.expectedValue) {
+            return
+        }
+    }
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
     if s.TokenLiteral() != "let" {
         t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
@@ -79,37 +115,6 @@ func checkParserErrors(t *testing.T, p *Parser) {
         t.Errorf("parser error: %q", msg)
     }
     t.FailNow()
-}
-
-func TestReturnStatements(t *testing.T) {
-    input := `
-return 5;
-return 10;
-return 993322;
-`
-
-    l := lexer.New(input)
-    p := New(l)
-
-    program := p.ParseProgram()
-    checkParserErrors(t, p)
-
-    if len(program.Statements) != 3 {
-        t.Fatalf("program.Statements does not contain 3 statements. got=%d",
-            len(program.Statements))
-    }
-
-    for _, stmt := range program.Statements {
-        returnStmt, ok := stmt.(*ast.ReturnStatement)
-        if !ok {
-            t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
-            continue
-        }
-        if returnStmt.TokenLiteral() != "return" {
-            t.Errorf("returnStmt.TokenLiteral not 'return', got %q",
-                returnStmt.TokenLiteral())
-        }
-    }
 }
 
 func TestIdentifierExpression(t *testing.T) {
