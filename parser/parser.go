beb33cfc5fc54db05344715ae9c55bd678c15d78
Brian Ott <bott@b1-systems.de>
2025-02-10 15:27:41 +0100

    update: parseBoolean returning ast.Expression

--- parser/parser.go
+++ parser/parser.go
@@ -265,6 +265,7 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 }
 
 func (p *Parser) parseBoolean() ast.Expression {
+    return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
