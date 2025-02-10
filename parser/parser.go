c249d49e1cfbfd55558aaad4935c35dcf2e87445
Brian Ott <bott@b1-systems.de>
2025-02-10 15:24:32 +0100

    update: registered a prefixParseFn for token.FALSE - fixed typo

--- parser/parser.go
+++ parser/parser.go
@@ -57,7 +57,8 @@ func New(l *lexer.Lexer) *Parser {
     p.registerPrefix(token.INT, p.parseIntegerLiteral)
     p.registerPrefix(token.BANG, p.parsePrefixExpression)
     p.registerPrefix(token.MINUS, p.parsePrefixExpression)
-    p.registerPrefix(token.TURE, p.parseBoolean)
+    p.registerPrefix(token.TRUE, p.parseBoolean)
+    p.registerPrefix(token.FALSE, p.parseBoolean)
 
     p.infixParseFns = make(map[token.TokenType]infixParseFn)
     p.registerInfix(token.PLUS, p.parseInfixExpression)
