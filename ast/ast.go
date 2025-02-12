b0950b1f08ae628759da94bf9ced7f6c23f4d168
Brian Ott <bott@b1-systems.de>
2025-02-12 19:24:39 +0100

    update: moved funcs and struct Boolean

--- ast/ast.go
+++ ast/ast.go
@@ -116,6 +116,15 @@ func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string { return i.Value }
 
+type Boolean struct {
+    Token   token.Token
+    Value   bool
+}
+
+func (b *Boolean) expressionNode()  {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string { return b.Token.Literal }
+
 type IntegerLiteral struct {
     Token   token.Token
     Value   int64
@@ -163,12 +172,3 @@ func (ie *InfixExpression) String() string {
 
     return out.String()
 }
-
-type Boolean struct {
-    Token   token.Token
-    Value   bool
-}
-
-func (b *Boolean) expressionNode()  {}
-func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
-func (b *Boolean) String() string { return b.Token.Literal }
