f14068c7b43d23ab6cf6d0e33859cca6c1e6c8ea
Brian Ott <bott@b1-systems.de>
2025-02-16 18:41:09 +0100

    update: assert that stmt.Expression is ast.IfExpression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -456,6 +456,11 @@ func TestIfElseExpression(t *testing.T) {
         t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
             program.Statements[0])
     }
+
+    exp, ok := stmt.Expression.(*ast.IfExpression)
+    if !ok {
+        t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T", stmt.Expression)
+    }
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
