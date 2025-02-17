43c73d82b46a5f78a98f381cd9033ae66810b6b3
Brian Ott <bott@b1-systems.de>
2025-02-17 16:29:18 +0100

    update: assert that there's an infix expression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -461,6 +461,10 @@ func TestIfElseExpression(t *testing.T) {
     if !ok {
         t.Fatalf("stmt.Expression is not ast.IfExpression. got=%T", stmt.Expression)
     }
+
+    if !testInfixExpression(t, exp.Condition, "x", "<", "y") {
+        return
+    }
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
