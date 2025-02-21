72bb0c425307a7fd20647507d804033c1bb18ad3
Brian Ott <bott@b1-systems.de>
2025-02-21 17:42:10 +0100

    update: defined func signature String to fulfil interfaces

--- ast/ast.go
+++ ast/ast.go
@@ -224,4 +224,18 @@ type FunctionLiteral struct {
 func (fl *FunctionLiteral) expressionNode()     {}
 func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
 func (fl *FunctionLiteral) String() string {
+    var out bytes.Buffer
+
+    params := []string{}
+    for _, p := range fl.Parameters {
+        params = append(params, p.String())
+    }
+
+    out.WriteString(fl.TokenLiteral())
+    out.WriteString("(")
+    out.WriteString(strings.Join(params, ", "))
+    out.WriteString(") ")
+    out.WriteString(fl.Body.String())
+
+    return out.String()
 }
