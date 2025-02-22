907b784b7ef6d6135ef4e8b8bd088e8a5bfa8296
Brian Ott <bott@b1-systems.de>
2025-02-22 16:45:33 +0100

    update: defined func signature TestFunctionLiteral

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -497,6 +497,9 @@ func TestIfElseExpression(t *testing.T) {
     }
 }
 
+func TestFunctionLiteral(t *testing.T) {
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
     if s.TokenLiteral() != "let" {
         t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
