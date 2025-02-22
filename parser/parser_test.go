cf6bc2262e60734908999fa9d9bead19eac069ae
Brian Ott <bott@b1-systems.de>
2025-02-22 16:48:07 +0100

    update: assert that program.Statements contains 1

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -504,6 +504,11 @@ func TestFunctionLiteral(t *testing.T) {
     p := New(l)
     program := p.ParseProgram()
     checkParserErrors(t, p)
+
+    if len(program.Statements) != 1 {
+        t.Fatalf("program.Statements does not contain %d statements. got=%d\n",
+            1, len(program.Statements))
+    }
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
