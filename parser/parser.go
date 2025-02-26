58504c2a3bddae7e36d7613ce0a75da7a33e2bd6
Brian Ott <bott@b1-systems.de>
2025-02-26 20:33:24 +0100

    update: parseFunctionParameters constructs the slice of params

--- parser/parser.go
+++ parser/parser.go
@@ -352,6 +352,30 @@ func (p *Parser) parseFunctionLiteral() ast.Expression {
 }
 
 func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+    identifiers := []*ast.Identifier{}
+
+    if p.peekTokenIs(token.RPAREN) {
+        p.nextToken()
+        return identifiers
+    }
+
+    p.nextToken
+
+    ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+    identifiers = append(identifiers, ident)
+
+    for p.peekTokenIs(token.COMMA) {
+        p.nextToken()
+        p.nextToken()
+        ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+        identifiers = append(identifiers, ident)
+    }
+
+    if !p.expectPeek(token.RPAREN) {
+        return nil
+    }
+
+    return identifiers
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
