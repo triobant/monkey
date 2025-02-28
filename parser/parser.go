272d1f57daee70352261f1653eb27c12dc086060
Brian Ott <bott@b1-systems.de>
2025-02-28 11:56:46 +0100

    update: style

--- parser/parser.go
+++ parser/parser.go
@@ -80,19 +80,37 @@ func New(l *lexer.Lexer) *Parser {
     return p
 }
 
-func (p *Parser) peekError(t token.TokenType) {
-    msg := fmt.Sprintf("expected next token to be %s, got %s instead",
-        t, p.peekToken.Type)
-    p.errors = append(p.errors, msg)
-}
-
 func (p *Parser) nextToken() {
     p.curToken = p.peekToken
     p.peekToken = p.l.NextToken()
 }
 
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+    return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+    return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+    if p.peekTokenIs(t) {
+        p.nextToken()
+        return true
+    } else {
+        p.peekError(t)
+        return false
+    }
+}
+
 func (p *Parser) Errors() []string {
     return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+        t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -376,24 +394,6 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
     }
 
     return identifiers
-}
-
-func (p *Parser) curTokenIs(t token.TokenType) bool {
-    return p.curToken.Type == t
-}
-
-func (p *Parser) peekTokenIs(t token.TokenType) bool {
-    return p.peekToken.Type == t
-}
-
-func (p *Parser) expectPeek(t token.TokenType) bool {
-    if p.peekTokenIs(t) {
-        p.nextToken()
-        return true
-    } else {
-        p.peekError(t)
-        return false
-    }
 }
 
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
