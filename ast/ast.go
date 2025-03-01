aebd2a7da459f6405063e184bc02ba0eaba721ff
Brian Ott <bott@b1-systems.de>
2025-03-01 21:17:46 +0100

    update: defined func signature String to fulfil interfaces

--- ast/ast.go
+++ ast/ast.go
@@ -249,3 +249,18 @@ type CallExpression struct {
 
 func (ce *CallExpression) expressionNode()      {}
 func (ce *CallExpression) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpression) String() string {
+    var out bytes.Buffer
+
+    args := []string{}
+    for _, a := range ce.Arguments {
+        args = append(args, a.String())
+    }
+
+    out.WriteString(ce.Function.String())
+    out.WriteString("(")
+    out.WriteString(strings.Join(args, ", "))
+    out.WriteString(")")
+
+    return out.String()
+}
