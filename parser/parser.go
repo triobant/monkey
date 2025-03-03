add20b32293cdba88f31edbb2dd8d36f1ec50254
Brian Ott <bott@b1-systems.de>
2025-03-03 20:39:55 +0100

    update: return expression after calling parseCallArguments function

--- parser/parser.go
+++ parser/parser.go
@@ -399,6 +399,9 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
 }
 
 func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+    exp := &ast.CallExpression{Token: p.curToken, Function: function}
+    exp.Arguments = p.parseCallArguments()
+    return exp
 }
 
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
