7c8bc452d11c994d88d5ea278c02701d6fa5955b
Brian Ott <bott@b1-systems.de>
2025-03-04 12:43:25 +0100

    update: removed TODO - skipping SEMICOLON token

--- parser/parser.go
+++ parser/parser.go
@@ -172,9 +172,10 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
     stmt := &ast.ReturnStatement{Token: p.curToken}
 
     p.nextToken()
-    // TODO: We're skipping the expressions until we
-    // encounter a semicolon
-    for !p.curTokenIs(token.SEMICOLON) {
+
+    stmt.ReturnValue = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
