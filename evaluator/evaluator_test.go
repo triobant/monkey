1faaa137f300a9082c8319e7bdaf512bdf184136
Brian Ott <bott@b1-systems.de>
2025-03-12 19:23:32 +0100

    update: defined test cases for TestEvalIntegerExpression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,4 +8,11 @@ import (
 )
 
 func TestEvalIntegerExpression(t *testing.T) {
+    tests := []struct {
+        input       string
+        expected    int64
+    }{
+        {"5", 5},
+        {"10", 10},
+    }
 }
