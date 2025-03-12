dab54ce73fa309c21de547e796f472c2a97e1fe4
Brian Ott <bott@b1-systems.de>
2025-03-12 19:18:47 +0100

    update: defined Type and Inspect methods for null object

--- object/object.go
+++ object/object.go
@@ -29,3 +29,6 @@ func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
