897908875be452b96a6d9d975b1fc98e24f9c420
Brian Ott <bott@b1-systems.de>
2025-03-13 19:14:16 +0100

    update: comments for clarity

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,8 +7,12 @@ import (
 
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
+
+    // Statements
+    // Expressions
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
+
     }
 
     return nil
