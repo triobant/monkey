399dfd1ca2053691824596c3d3c6377d630059d4
Brian Ott <bott@b1-systems.de>
2025-03-13 19:16:12 +0100

    update: more cases for Statements *ast.Program and *ast.ExpressionStatement

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -9,6 +9,12 @@ func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
 
     // Statements
+    case *ast.Program:
+        return evalStatements(node.Statements)
+
+    case *ast.ExpressionStatement:
+        return Eval(node.Expression)
+
     // Expressions
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
