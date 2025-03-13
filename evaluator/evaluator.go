390bd4ba70acbdaee694308364a910756cd03dd2
Brian Ott <bott@b1-systems.de>
2025-03-13 19:12:18 +0100

    update: switch case statement in Eval function

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,4 +6,10 @@ import (
 )
 
 func Eval(node ast.Node) object.Object {
+    switch node := node.(type) {
+    case *ast.IntegerLiteral:
+        return &object.Integer{Value: node.Value}
+    }
+
+    return nil
 }
