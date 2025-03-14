f6ee52195427c073b009de45b3234f934f8a0429
Brian Ott <bott@b1-systems.de>
2025-03-14 21:27:32 +0100

    update: evalStatements iterates over slice of statements returning an evaluated object.Object

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -25,4 +25,11 @@ func Eval(node ast.Node) object.Object {
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
+    var result object.Object
+
+    for _, statement := range stmts {
+        result = Eval(statement)
+    }
+
+    return result
 }
