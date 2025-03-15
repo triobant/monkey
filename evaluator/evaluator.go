e5d0b37ff77e4292655994925f249fd06097c1dd
Brian Ott <bott@b1-systems.de>
2025-03-16 00:26:52 +0100

    update: defined references for only two possible bool

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,11 @@ import (
     "monkey/object"
 )
 
+var (
+    TRUE  = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
 
