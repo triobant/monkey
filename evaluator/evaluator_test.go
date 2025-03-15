cb275015918b8aaf018101eb5e1094d4b76c55ab
Brian Ott <bott@b1-systems.de>
2025-03-15 21:46:25 +0100

    update: assert that object is an Integer

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -31,4 +31,9 @@ func testEval(input string) object.Object {
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+    result, ok := obj.(*object.Integer)
+    if !ok {
+        t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+        return false
+    }
 }
