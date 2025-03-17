0273590d596acb87226abcb5b6922df8b59d4019
Brian Ott <bott@b1-systems.de>
2025-03-17 12:20:24 +0100

    update: defined new case for *ast.PrefixExpression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -27,6 +27,10 @@ func Eval(node ast.Node) object.Object {
 
     case *ast.Boolean:
         return nativeBoolToBooleanObject(node.Value)
+
+    case *ast.PrefixExpression:
+        right := Eval(node.Right)
+        return evalPrefixExpression(node.Operator, right)
     }
 
     return nil
