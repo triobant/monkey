2cd8611c6df15cd812e70ddb7d4bed53076ce1dd
Brian Ott <bott@b1-systems.de>
2025-03-20 13:05:04 +0100

    update: switch case for right object.Object

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -63,4 +63,14 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 }
 
 func evalBangOperatorExpression(right object.Object) object.Object {
+    switch right {
+    case TRUE:
+        return FALSE
+    case FALSE:
+        return TRUE
+    case NULL:
+        return TRUE
+    default:
+        return FALSE
+    }
 }
