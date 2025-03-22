260f4e85f723edd25d2b827b3b9c4af267f3a24a
Brian Ott <bott@b1-systems.de>
2025-03-22 20:55:27 +0100

    update: switch case statements for different operators

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -102,4 +102,19 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 }
 
 func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+    leftVal := left.(*object.Integer).Value
+    rightVal := right.(*object.Integer).Value
+
+    switch operator {
+    case "+":
+        return &object.Integer{Value: leftVal + rightVal}
+    case "-":
+        return &object.Integer{Value: leftVal - rightVal}
+    case "*":
+        return &object.Integer{Value: leftVal * rightVal}
+    case "/":
+        return &object.Integer{Value: leftVal / rightVal}
+    default:
+        return NULL
+    }
 }
