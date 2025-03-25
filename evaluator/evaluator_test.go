47131324570ed35fa64c1abb420f480db0100c83
Brian Ott <bott@b1-systems.de>
2025-03-25 20:58:05 +0100

    update: defined test cases for TestIfElseExpressions

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -87,6 +87,18 @@ func TestBangOperator(t *testing.T) {
 }
 
 func TestIfElseExpresions(t *testing.T) {
+    tests := []struct {
+        input       string
+        expected    interface{}
+    }{
+        {"if (true) { 10 }", 10},
+        {"if (false) { 10 }", nil},
+        {"if (1) { 10 }", 10},
+        {"if (1 < 2) { 10 }", 10},
+        {"if (1 > 2) { 10 }", nil},
+        {"if (1 > 2) { 10 } else { 20 }", 20},
+        {"if (1 < 2) { 10 } else { 20 }", 10},
+    }
 }
 
 func testEval(input string) object.Object {
