5183e7a1bb28a5ac4537825cab48d9d9b41af35b
Brian Ott <bott@b1-systems.de>
2025-03-25 21:00:37 +0100

    update: iterating over test cases calling testIntegerObject and testNullObject

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -99,6 +99,16 @@ func TestIfElseExpresions(t *testing.T) {
         {"if (1 > 2) { 10 } else { 20 }", 20},
         {"if (1 < 2) { 10 } else { 20 }", 10},
     }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        integer, ok := tt.expected.(int)
+        if ok {
+            testIntegerObject(t, evaluated, int64(integer))
+        } else {
+            testNullObject(t, evaluated)
+        }
+    }
 }
 
 func testEval(input string) object.Object {
