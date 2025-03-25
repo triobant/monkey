c4b91fad7496efb1043237ba9f0142554a7ca10a
Brian Ott <bott@b1-systems.de>
2025-03-25 21:02:52 +0100

    update: asserting that obj is NULL

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -149,4 +149,9 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 }
 
 func ( testNullObject(t *testing.T, obj object.Object) bool {
+    if obj != NULL {
+        t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
+        return false
+    }
+    return true
 }
