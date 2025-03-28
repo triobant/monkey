759247686ce00ca9c87110a17b03c3381e880cbf
Brian Ott <bott@b1-systems.de>
2025-03-28 20:33:05 +0100

    update: iterating over statements evaluating them returning a result object.Object

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -164,4 +164,14 @@ func isTruthy(obj object.Object) bool {
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
+    var result object.Object
+
+    for _, statement := range stmts {
+        result = Eval(statement)
+
+        if returnValue, ok := result.(*object.ReturnValue); ok {
+            return returnValue.Value
+        }
+    }
+    return result
 }
