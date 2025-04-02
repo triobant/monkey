770841c9cc19fff1b5184d999f0bf62acca395d3
Brian Ott <bott@b1-systems.de>
2025-04-02 22:42:38 +0200

    update: asserting errObj is an error object and errObj.Message has the right message

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -182,6 +182,22 @@ if (10 > 1) {
             "unknown operator: BOOLEAN + BOOLEAN",
         },
     }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+
+        errObj, ok := evaluated.(*object.Error)
+        if !ok {
+            t.Errorf("no error object returned. got=%T(%+v)",
+                evaluated, evaluated)
+            continue
+        }
+
+        if errObj.Message != tt.expectedMessage {
+            t.Errorf("wrong error message. expected=%q, got=%q",
+                tt.expectedMessage, errObj.Message)
+        }
+    }
 }
 
 func testEval(input string) object.Object {
