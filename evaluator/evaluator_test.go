19a5effc20938eb9b3b1bcadb75fb0e043c3f9a1
Brian Ott <bott@b1-systems.de>
2025-04-07 21:14:48 +0200

    update: defined test cases for TestLetStatement

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -201,6 +201,15 @@ if (10 > 1) {
 }
 
 func TestLetStatements(t *testing.T) {
+    tests := []struct {
+        input       string
+        expected    int64
+    }{
+        {"let a = 5; a;" 5},
+        {"let a = 5 * 5; a;" 25},
+        {"let a = 5; let b = a; b;"" 5},
+        {"let a = 5; let b = a; let c = a + b + 5; c;" 15},
+    }
 }
 
 func testEval(input string) object.Object {
