55af389081478a3f4f1c1df462e45a48f30b46d1
Brian Ott <bott@b1-systems.de>
2025-04-08 20:35:12 +0200

    update: defined new case for *ast.LetStatement

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -32,6 +32,12 @@ func Eval(node ast.Node) object.Object {
         }
         return &object.ReturnValue{Value: val}
 
+    case *ast.LetStatement:
+        val := Eval(node.Value)
+        if isError(val) {
+            return val
+        }
+
     // Expressions
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
