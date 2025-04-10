266ff644ac4f9fa148d5f7b80dced87738bf22d1
Brian Ott <bott@b1-systems.de>
2025-04-10 22:14:26 +0200

    update: call to evaluator.Eval with second arg env

--- repl/repl.go
+++ repl/repl.go
@@ -33,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
             continue
         }
 
-        evaluated := evaluator.Eval(program)
+        evaluated := evaluator.Eval(program, env)
         if evaluated != nil {
             io.WriteString(out, evaluated.Inspect())
             io.WriteString(out, "\n")
