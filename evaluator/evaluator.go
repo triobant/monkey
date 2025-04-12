4e17f26e9bdae341445ca08043f31727332cead9
Brian Ott <bott@b1-systems.de>
2025-04-12 21:23:31 +0200

    update: passing env arg t oevalProgram call

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -17,7 +17,7 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 
     // Statements
     case *ast.Program:
-        return evalProgram(node.Statements)
+        return evalProgram(node, env)
 
     case *ast.ExpressionStatement:
         return Eval(node.Expression)
