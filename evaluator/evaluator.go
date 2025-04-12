ebbf7a1d250e6048211e7617e65672911a5fe3ed
Brian Ott <bott@b1-systems.de>
2025-04-12 21:12:04 +0200

    update: checking a value associated with given name in current enviironment

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -241,4 +241,10 @@ func evalIdentifier(
     node *ast.Identifier
     env *object.Environment,
 ) object.Object {
+    val, ok := env.Get(node.Value)
+    if !ok {
+        return newError("identifier not found: " + node.Value)
+    }
+
+    return val
 }
