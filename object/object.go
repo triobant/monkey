6c01b35a91d561bb99e78e473a8da854f284e518
Brian Ott <bott@b1-systems.de>
2025-04-14 18:14:52 +0200

    update: new constant var FUNCTION_OBJ

--- object/object.go
+++ object/object.go
@@ -12,9 +12,10 @@ type ObjectType string
 const (
     INTEGER_OBJ = "INTEGER"
     BOOLEAN_OBJ = "BOOLEAN"
-    NULL_OBJ    = "NULL"
+    NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
-    ERROR_OBJ   = "ERROR"
+    ERROR_OBJ = "ERROR"
+    FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface {
