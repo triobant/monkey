b4a048ed8aa5a7d1bcc462e406a726f5e5aca386
Brian Ott <bott@b1-systems.de>
2025-04-18 20:47:27 +0200

    update: defined new case for *ast.CallExpression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -72,6 +72,12 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
         body := node.Body
         return &object.Function{Parameters: params, Env: env, Body: body}
 
+    case *ast.CallExpression:
+        function := Eval(node.Function, env)
+        if isError(function) {
+            return function
+        }
+
     }
 
 
