6104c6be79ad6baa8d3b53ae80eade018451f8f0
Brian Ott <bott@b1-systems.de>
2025-04-19 18:41:38 +0200

    update: iterating over a list of ast.Expressions evaluating them in context of current env returning a list of object.Object

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -274,4 +274,15 @@ func evalExpressions(
     exps    []ast.Expression,
     env     *object.Environment,
 ) []object.Object {
+    var result []object.Object
+
+    for _, e := range exps {
+        evaluated := Eval(e, env)
+        if isError(evaluated) {
+            return []object.Object{evaluated}
+        }
+        result = append(result, evaluated)
+    }
+
+    return result
 }
