b87c01ab830f3f6038bd9810f064f0c245718ce9
Brian Ott <bott@b1-systems.de>
2025-05-03 22:58:19 +0200

    upate: new testcase for TestReutnrStatements

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -120,6 +120,7 @@ func TestReturnStatements(t *testing.T) {
         {"return 10; 9;", 10},
         {"return 2 * 5; 9;", 10},
         {"9; return 2 * 5; 9;", 10},
+        {"if (10 > 1) { return 10; }", 10},
         {
             `
 if (10 > 1) {
