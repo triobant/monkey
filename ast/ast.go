97e1d2109cd03da13a5d02770b5ab1da6b02de13
Brian Ott <bott@b1-systems.de>
2025-05-11 22:44:26 +0200

    update: defined String method for StringLiteral

--- ast/ast.go
+++ ast/ast.go
@@ -272,3 +272,4 @@ type StringLiteral struct {
 
 func (sl *StringLiteral) expressionNode()       {}
 func (sl *StringLiteral) TokenLiteral() string  { return sl.Token.Literal }
+func (sl *StringLiteral) String() string { return sl.Token.Literal }
