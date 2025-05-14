e95d06fb3089858e1a367b713eb945e5b57950f8
Brian Ott <bott@b1-systems.de>
2025-05-14 22:11:56 +0200

    update: const STRING_OBJ

--- object/object.go
+++ object/object.go
@@ -12,6 +12,7 @@ type ObjectType string
 const (
     INTEGER_OBJ = "INTEGER"
     BOOLEAN_OBJ = "BOOLEAN"
+    STRING_OBJ = "STRING"
     NULL_OBJ = "NULL"
     RETURN_VALUE_OBJ = "RETURN_VALUE"
     ERROR_OBJ = "ERROR"
