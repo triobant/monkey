a1fb448890991e8daa8d744abe2ff868b78c7207
Brian Ott <bott@b1-systems.de>
2025-05-17 21:43:05 +0200

    update: defined func signature TestErrorHandling

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -332,6 +332,9 @@ func TestStringConcatenation(t *testing.T) {
     }
 }
 
+func TestErrorHandling(t *testing.T) {
+}
+
 func testEval(input string) object.Object {
     l := lexer.New(input)
     p := parser.New(l)
