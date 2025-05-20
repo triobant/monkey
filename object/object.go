72d53409989af9bcf1ef7793249f703b41c33ac8
Brian Ott <bott@b1-systems.de>
2025-05-20 23:01:39 +0200

    update: defined Builtin struct obj

--- object/object.go
+++ object/object.go
@@ -89,3 +89,7 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+    Fn BuiltinFunction
+}
