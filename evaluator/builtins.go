7dd2c8e65656610158b79129d662bae906ebe85f
Brian Ott <bott@b1-systems.de>
2025-05-23 21:52:56 +0200

    update: switch case statement for *object.String

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -11,6 +11,11 @@ var builtins = map[string]*object.Builtin{
                 return newError("wrong number of arguments. got=%d, want=1",
                     len(args))
             }
+
+            switch arg := args[0].(type) {
+            case *object.String:
+                return &object.Integer{Value: int64(len(arg.Value))}
+            }
         },
     },
 }
