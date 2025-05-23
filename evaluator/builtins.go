dee4e8efc334219176121bd15792699d4b93fb19
Brian Ott <bott@b1-systems.de>
2025-05-23 21:53:53 +0200

    update: default case returning error

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -15,6 +15,9 @@ var builtins = map[string]*object.Builtin{
             switch arg := args[0].(type) {
             case *object.String:
                 return &object.Integer{Value: int64(len(arg.Value))}
+            default:
+                return newError("argument to `len` not supported, got %s",
+                    args[0].Type())
             }
         },
     },
