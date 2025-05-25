097d0a2c8cb0d715d7b88441615fd7495fb0f707
Brian Ott <bott@b1-systems.de>
2025-05-25 23:09:30 +0200

    update: defined String method for ArrayLiteral

--- ast/ast.go
+++ ast/ast.go
@@ -281,3 +281,17 @@ type ArrayLiteral struct {
 
 func (al *ArrayLiteral) expressionNode()      {}
 func (al *ArrayLiteral) TokenLiteral() string { return al.Token.Literal }
+func (al *ArrayLiteral) String() string {
+    var out bytes.Buffer
+
+    elements := []string{}
+    for _, el := range al.Elements {
+        elements = append(elements, el.String())
+    }
+
+    out.WriteString("[")
+    out.WriteString(strings.Join(elements, ", "))
+    out.WriteString("]")
+
+    return out.String()
+}
