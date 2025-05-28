6128aafca201173dc5cb5e8ebbf9a2d4f8659d66
Brian Ott <bott@b1-systems.de>
2025-05-28 20:52:37 +0200

    update: iterating through array elements till ']' appears and returning a slice of ast.Expression

--- parser/parser.go
+++ parser/parser.go
@@ -420,6 +420,27 @@ func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
 }
 
 func (p *Parser) parseExpressionList(end token.TokenType) []ast.Expression {
+    list := []ast.Expression{}
+
+    if p.peekTokenIs(end) {
+        p.nextToken()
+        return list
+    }
+
+    p.nextToken()
+    list = append(list, p.parseExpression(LOWEST))
+
+    for p.peekTokenIs(token.COMMA) {
+        p.nextToken()
+        p.nextToken()
+        list = append(list, p.parseExpression(LOWEST))
+    }
+
+    if !p.expectPeek(end) {
+        return nil
+    }
+
+    return list
 }
 
 func (p *Parser) parseArrayLiteral() ast.Expression {
