5b6ff30fd13d9ccb30c9a6a7a5f61bda53daa1e4
Brian Ott <bott@b1-systems.de>
2025-05-30 23:11:18 +0200

    update: defined String method for IndexExpression

--- ast/ast.go
+++ ast/ast.go
@@ -304,3 +304,14 @@ type IndexExpression struct {
 
 func (ie *IndexExpression) expressionNode()      {}
 func (ie *IndexExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *IndexExpression) String() string {
+    var out bytes.Buffer
+
+    out.WriteString("(")
+    out.WriteString(ie.Left.String())
+    out.WriteString("[")
+    out.WriteString(ie.Index.String())
+    out.WriteString("])")
+
+    return out.String()
+}
