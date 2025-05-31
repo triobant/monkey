15e771d3beaf7a5f175bd9a8ce4feefa9e2e86f5
Brian Ott <bott@b1-systems.de>
2025-05-31 21:13:20 +0200

    update: returning expression when RBRACKET is found

--- parser/parser.go
+++ parser/parser.go
@@ -453,6 +453,16 @@ func (p *Parser) parseArrayLiteral() ast.Expression {
 }
 
 func (p *Parser) parseIndexExpression(left ast.Expression) ast.Expression {
+    exp := &ast.IndexExpression{Token: p.curToken, Left: left}
+
+    p.nextToken()
+    exp.Index = p.parseExpression(LOWEST)
+
+    if !p.expectPeek(token.RBRACKET) {
+        return nil
+    }
+
+    return exp
 }
 
 func (p *Parser) parseCallArguments() []ast.Expression {
