94f619d61d5e50e28328ff24d4f421ace1cfe400
Brian Ott <bott@b1-systems.de>
2025-06-01 18:13:17 +0200

    update: defined method Inspect for Array object

--- object/object.go
+++ object/object.go
@@ -107,3 +107,17 @@ type Array struct {
 }
 
 func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string  {
+    var out bytes.Buffer
+
+    elements := []string{}
+    for _, e := range ao.Elements {
+        elements = append(elements, e.Inspect())
+    }
+
+    out.WriteString("[")
+    out.WriteString(strings.Join(elements, ", "))
+    out.WriteString("]")
+
+    return out.String()
+}
