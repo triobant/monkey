3518de8147768fb2a48569f9b7cdc7fd46444cda
Brian Ott <bott@b1-systems.de>
2025-06-03 23:16:11 +0200

    update: checking if idx is out of range returning NULL otherwise the desired element

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -380,4 +380,10 @@ func evalArrayIndexExpression(array, index object.Object) object.Object {
     arrayObject := array.(*object.Array)
     idx := index.(*object.Integer).Value
     max := int64(len(arrayObject.Elements) - 1)
+
+    if idx < 0 || idx > max {
+        return NULL
+    }
+
+    return arrayObject.Elements[idx]
 }
