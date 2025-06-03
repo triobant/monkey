08ce6801190e6b9ea04866dd3683009f25cae68a
Brian Ott <bott@b1-systems.de>
2025-06-03 23:11:19 +0200

    update: switch case statement for evalINdexExpression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -368,4 +368,10 @@ func unwrapReturnValue(obj object.Object) object.Object {
 }
 
 func evalIndexExpression(left, index object.Object) object.Object {
+    switch {
+    case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+        return evalArrayIndexExpression(left, index)
+    default:
+        return newError("index operator not supported: %s", left.Type())
+    }
 }
