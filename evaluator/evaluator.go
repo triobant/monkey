6faeb276fe04a89943305f9a1b78791954349da5
Brian Ott <bott@b1-systems.de>
2025-06-03 23:14:48 +0200

    update: defined arrayOjbect, idx and max

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -377,4 +377,7 @@ func evalIndexExpression(left, index object.Object) object.Object {
 }
 
 func evalArrayIndexExpression(array, index object.Object) object.Object {
+    arrayObject := array.(*object.Array)
+    idx := index.(*object.Integer).Value
+    max := int64(len(arrayObject.Elements) - 1)
 }
