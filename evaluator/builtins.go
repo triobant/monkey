b23dea811179f8331000c67c71ebb0a19783b3ac
Brian Ott <bott@b1-systems.de>
2025-06-05 23:12:21 +0200

    update: assert that args of builtin func last contains correct num of arguments

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -43,6 +43,11 @@ var builtins = map[string]*object.Builtin{
         },
     },
     "last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
 		},
 	},
 }
