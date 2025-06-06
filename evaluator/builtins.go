eceb42c1eeb78549d29d5df16da256017a4b4afa
Brian Ott <bott@b1-systems.de>
2025-06-06 23:36:50 +0200

    update: assert that args of builtin func 'rest' contains correct num of arguments

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -64,6 +64,10 @@ var builtins = map[string]*object.Builtin{
 	},
     "rest": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
 		},
 	},
 }
