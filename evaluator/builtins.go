2bd90985d1e2060579d4aae0f5357976ac145375
Brian Ott <bott@b1-systems.de>
2025-06-09 22:51:21 +0200

    update: puts returning string representations of args passed in builtin function 'puts'

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -25,6 +25,11 @@ var builtins = map[string]*object.Builtin{
     },
     "puts": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
     "first": &object.Builtin{
