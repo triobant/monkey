27170f99f4c50856d7caf7ec912371f5a67ab2dc
Brian Ott <bott@b1-systems.de>
2025-06-18 23:10:15 +0200

    update: registered new prefix token LBRACE

--- parser/parser.go
+++ parser/parser.go
@@ -67,6 +67,7 @@ func New(l *lexer.Lexer) *Parser {
     p.registerPrefix(token.IF, p.parseIfExpression)
     p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
     p.registerPrefix(token.LBRACKET, p.parseArrayLiteral)
+    p.registerPrefix(token.LBRACE, p.parseHashLiteral)
 
     p.infixParseFns = make(map[token.TokenType]infixParseFn)
     p.registerInfix(token.PLUS, p.parseInfixExpression)
