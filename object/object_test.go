abac7e5a3568d49ab9c195f6a359b8a0b4bc5605
Brian Ott <bott@b1-systems.de>
2025-06-20 22:42:14 +0200

    update: defined vars for TestStringHashKey

--- object/object_test.go
+++ object/object_test.go
@@ -3,4 +3,8 @@ package object
 import "testing"
 
 func TestStringHashKey(t *testing.T) {
+    hello1 := &String{Value: "Hello World"}
+    hello2 := &String{Value: "Hello World"}
+    diff1 := &String{Value: "My name is johnny"}
+    diff2 := &String{Value: "My name is johnny"}
 }
