9892222a2789b90bbb91017ce4702b4bec0b9171
Brian Ott <bott@b1-systems.de>
2025-06-21 23:16:48 +0200

    update: HashKey functon returns HashKey object with 'scope' to other object types

--- object/object.go
+++ object/object.go
@@ -129,4 +129,13 @@ type HashKey struct {
 }
 
 func (b *Boolean) HashKey() HashKey {
+    var value uint64
+
+    if b.Value {
+        value = 1
+    } else {
+        value = 0
+    }
+
+    return HashKey{Type: b.Type(), Value: value}
 }
