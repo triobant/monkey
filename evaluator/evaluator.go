1d7653983d8dee8bd29a1dee6e73b7b44787f3f1
Brian Ott <bott@b1-systems.de>
2025-07-07 09:36:47 +0200

    update: case for hash object calling evalHashIndexExpression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -374,6 +374,8 @@ func evalIndexExpression(left, index object.Object) object.Object {
     switch {
     case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
         return evalArrayIndexExpression(left, index)
+    case left.Type() == object.HASH_OBJ:
+        return evalHashIndexExpression(left, index)
     default:
         return newError("index operator not supported: %s", left.Type())
     }
