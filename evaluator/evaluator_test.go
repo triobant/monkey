3560823ad1d4529a0ce49c5f303598c3e44f0799
Brian Ott <bott@b1-systems.de>
2025-07-09 23:14:03 +0200

    update: call to testIntegerObject

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -317,6 +317,15 @@ ourFunction(20) + first + second;`
 }
 
 func TestClosures(t *testing.T) {
+    input := `
+let newAdder = fn(x) {
+    fn(y) { x + y };
+};
+
+let addTwo = newAdder(2);
+addTwo(2);`
+
+    testIntegerObject(t, testEval(input), 4)
 }
 
 func TestStringLiteral(t *testing.T) {
