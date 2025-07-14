0e1aa0073640cea602a9bd54d7d73fc3cf90e505
Brian Ott <bott@b1-systems.de>
2025-07-14 22:07:41 +0200

    update: fixed missing dot

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -6,7 +6,7 @@ import (
 
 var builtins = map[string]*object.Builtin{
     "len": &object.Builtin{
-        Fn: func(args ..object.Object) object.Object {
+        Fn: func(args ...object.Object) object.Object {
             if len(args) != 1 {
                 return newError("wrong number of arguments. got=%d, want=1",
                     len(args))
